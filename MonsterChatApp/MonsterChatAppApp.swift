c8214f8b5ef6b282c663cc0dd44fe4b17f912d79
obaro paul <obaroiskool@gmail.com>
2024-06-30 11:51:16 +0100

    Initial Commit

---
+++ MonsterChatApp/MonsterChatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MonsterChatAppApp.swift
+//  MonsterChatApp
+//
+//  Created by Obaro Paul on 30/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct MonsterChatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
